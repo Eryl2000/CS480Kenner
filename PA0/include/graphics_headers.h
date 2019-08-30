77634a82d733e06f17c06dac0784d5b9bb2e1f8f
Eryl Kenner <eryl.kenner@gmail.com>
2019-08-30 09:36:42 -0700

    Added line to allow experimental code

--- PA0/include/graphics_headers.h
+++ PA0/include/graphics_headers.h
@@ -15,6 +15,7 @@
 
 // GLM for matricies
 #define GLM_FORCE_RADIANS
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
