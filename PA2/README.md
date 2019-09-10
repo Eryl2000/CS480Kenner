0c95df3678a709ff3263aaf3f4d353fa7a9a2c24
Eryl Kenner <eryl.kenner@gmail.com>
2019-09-10 09:35:50 -0700

    Copied PA1 folder to PA2

---
+++ PA2/README.md
@@ -0,0 +1,11 @@
+# PA1: Spinning Cube
+This project reads vertex and fragement shaders from a file and uses them to make a cube rotate and orbit in space.
+## Running the Project
+
+```bash
+mkdir build
+cd build
+cmake ..
+make
+./Tutorial -v ../shaders/vertexShader.txt -f ../shaders/fragmentShader.txt
+```
---
+++ PA2/include/object.h
@@ -0,0 +1,32 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+
+class Object
+{
+  public:
+    Object();
+    ~Object();
+    void Update(unsigned int dt);
+    void Render();
+
+    glm::mat4 GetModel();
+
+  private:
+    glm::mat4 model;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+    
+    float turningRate;
+    float orbitRate;
+    float radius;
+
+    float angle;
+    float angleInOrbit;
+};
+
+#endif /* OBJECT_H */
