be4b977654adcc4ab1b9a0f2b8ec6d331a0b3abc
GarrettMoore22 <garrett22smoore@gmail.com>
2019-10-29 23:13:49 -0700

    tray with two spheres and a cylinder

--- PA8/src/graphics.cpp
+++ PA8/src/graphics.cpp
@@ -47,12 +47,22 @@ void Graphics::createObjects(int width, int height){
     dynamicsWorld->addRigidBody(m_camera->rigidbody, 1, 1);
 
     BaseObject *temp;
-    PhysicsOptions ps(true, ColliderType::Cube, PhysicsType::Dynamic);
+    PhysicsOptions ps(true, ColliderType::Sphere, PhysicsType::Dynamic);
     temp = new Movable(std::string("sphere"), NULL, std::string("../obj/newsphere.obj"), ps);
     objects.push_back(temp);
     dynamicsWorld->addRigidBody(temp->rigidbody, 1, 1);
 
     temp = new PhysicsObject(std::string("sphere"), NULL, std::string("../obj/newsphere.obj"), ps);
+    objects.push_back(temp);
+    dynamicsWorld->addRigidBody(temp->rigidbody, 1, 1);
+
+    PhysicsOptions cylinderPS(true, ColliderType::Cylinder, PhysicsType::Dynamic);
+    temp = new PhysicsObject(std::string("sphere"), NULL, std::string("../obj/newcylinder.obj"), cylinderPS);
+    objects.push_back(temp);
+    dynamicsWorld->addRigidBody(temp->rigidbody, 1, 1);
+
+    PhysicsOptions planePS(true, ColliderType::Plane, PhysicsType::Static);
+    temp = new PhysicsObject(std::string("sphere"), NULL, std::string("../obj/newtray.obj"), planePS);
     objects.push_back(temp);
     dynamicsWorld->addRigidBody(temp->rigidbody, 1, 1);
 }
